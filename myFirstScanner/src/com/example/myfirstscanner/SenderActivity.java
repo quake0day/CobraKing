317d596239656c81a5619222c459f3136853b9df
quake0day <quake0day@gmail.com>
2012-11-05 10:43:34 -0500

    update: now the user has the freedom to choose the desired qr-code version on the start-up screen this is the last modification before showing to boss tomorrow, if you want me to make any last-minute change, CALL ME 7189165391 my demo to the boss is tomorrow at 11 am

--- myFirstScanner/src/com/example/myfirstscanner/SenderActivity.java
+++ myFirstScanner/src/com/example/myfirstscanner/SenderActivity.java
@@ -7,13 +7,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class SenderActivity extends Activity {
+	
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sender);
+         // CheckBox c10= (CheckBox) this.findViewById(id.checkbox_10);
+          //CheckBox c14=(CheckBox) this.findViewById(id.checkbox_14);
+      /*  c10.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+        	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if ( isChecked )
+                {
+                    c14.toggle();
+                }
+            }
+        });
+        c14.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+        	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if ( isChecked )
+                {
+                    c10.toggle();
+                }
+            }
+        });*/
     }
 
     @Override
@@ -24,6 +48,9 @@ public class SenderActivity extends Activity {
     public void startEncoding(View view){
     	Intent intent=new Intent(this,EncoderActivity.class);
     	intent.putExtra("LONG_STRING",this.findViewById(id.input_message).toString());
+    	if(((CheckBox) this.findViewById(id.checkbox_10)).isChecked())
+    		intent.putExtra("version", 200);
+    	else intent.putExtra("version", 500);
     	this.startActivity(intent);
     }
 }
